267777e44b525ea58bb468c0369e55b43546052c
samuel <samuel.defosse44@gmail.com>
2022-03-06 14:07:10 +0100

    initialisation du menu

--- easycook/src/App.vue
+++ easycook/src/App.vue
@@ -1,17 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- MenuView -->
+    <MenuView />
+ 
+    <!-- Content -->
+    <div class="container">
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MenuView from "./components/MenuView.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    MenuView,
+  },
+};
 </script>
 
 <style>
@@ -21,6 +27,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 5px;
 }
 </style>
---
+++ easycook/src/components/MenuView.vue
@@ -0,0 +1,97 @@
+<template>
+  <nav class="navbar box" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a>
+        <img
+          class="navbar-brand-img"
+          src="../assets/logo-easy-cook.png"
+        />
+      </a>
+
+      <a
+        role="button"
+        class="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+        :class="{ 'is-active': mobileMenuActive }"
+        @click="mobileMenuActive = !mobileMenuActive"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div class="navbar-menu" :class="{ 'is-active': mobileMenuActive }">
+      <div class="navbar-start">
+        <router-link class="navbar-item" to="/">
+          Accueil
+        </router-link>
+        
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">
+            Menus
+          </a>
+          <div class="navbar-dropdown">
+            <router-link class="navbar-item">
+              Recette aléatoire
+            </router-link>
+            <router-link class="navbar-item">
+              Recherche recettes par nom
+            </router-link>
+            <router-link class="navbar-item">
+              Recherche recette par ingrédient
+            </router-link>
+            
+            <hr class="navbar-divider" />
+            <router-link class="navbar-item" to="/about">
+              A propos
+            </router-link>
+            <a
+              class="navbar-item"
+              href="https://github.com/samsamd/EasyCook"
+              target="github"
+            >
+              Voir le code source
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item copyright">
+          Created by Univ-Nantes &copy;
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "MenuView",
+  data() {
+    return {
+      mobileMenuActive: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.navbar {
+  height: 70px;
+}
+.navbar-brand-img {
+  margin-left: 10px;
+  height: 50px;
+  width: 60px;
+}
+
+.router-link-active {
+    font-weight: bold;
+    text-decoration: underline;
+    text-decoration-color: green;
+}
+</style>
